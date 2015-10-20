fd7b246d93b4c93840fd6ffd4982af0f437d5a12
zhasik92 <zhasik140192@mail.ru>
2015-10-21 00:09:05 +0300

    commit

---
+++ src/test/java/util/IdGeneratorTest.java
@@ -0,0 +1,14 @@
+package util;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Zhassulan on 20.10.2015.
+ */
+public class IdGeneratorTest {
+
+    @org.junit.Test
+    public void testGenerateId() throws Exception {
+
+    }
+}
